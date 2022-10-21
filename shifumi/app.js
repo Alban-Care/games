a4ac75f777b79837d58f8b3b6cc6443a82fd806e
Alban Caré <alban.care@gmail.com>
2022-10-22 00:22:39 +0200

    Première itération de l'algorythme du jeu et mise à jour du html

--- shifumi/app.js
+++ shifumi/app.js
@@ -36,3 +36,113 @@ let inGame = {
 /* Game default data configuration */
 
 let roundsGame = 3;
+
+/* ShiFuMi eventListener cards */
+let tradutionalPath = "#traditional>#cards>.card-group>";
+
+document
+  .querySelector(`${tradutionalPath}#shi`)
+  .addEventListener("click", () => {
+    inGame.round++;
+    ckeckResponses("shi");
+  });
+document
+  .querySelector(`${tradutionalPath}#fu`)
+  .addEventListener("click", () => {
+    inGame.round++;
+    ckeckResponses("fu");
+  });
+document
+  .querySelector(`${tradutionalPath}#mi`)
+  .addEventListener("click", () => {
+    inGame.round++;
+    ckeckResponses("mi");
+  });
+
+/* utils function */
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+/* intitialization functions */
+function gameInitialization() {
+  inGame.round = 1;
+  inGame.score.win = 0;
+  inGame.score.egality = 0;
+  inGame.score.lose = 0;
+  inGame.player = "";
+  inGame.computer = "";
+}
+
+function domUpdateOnGameLaunch() {
+  let btn = document.querySelector(".btn-play");
+  btn.setAttribute("disabled", "");
+
+  let overlay = document.querySelector("#traditional>.overlay");
+  overlay.remove();
+
+  displayCountRound(inGame.round);
+}
+
+/* Rounds functions */
+
+function getComputerCard() {
+  let index = getRandomInt(response.length);
+  return response[index];
+}
+
+function displayCountRound(round) {
+  document.querySelector(
+    "#traditional>#cards>h2"
+  ).textContent = `Round ${round} : select a card`;
+}
+
+function displayScore() {
+  document.querySelector(".win-item").textContent = inGame.score.win;
+  document.querySelector(".egality-item").textContent = inGame.score.egality;
+  document.querySelector(".lose-item").textContent = inGame.score.lose;
+}
+
+function displayRound() {
+  let li = document.createElement("li");
+  li.classList.add("round");
+
+  let strong = document.createElement("strong");
+  strong.classList.add("strong");
+  strong.textContent = `Round ${inGame.round} : `;
+
+  let player = document.createElement("span");
+  player.setAttribute("id", "player");
+  player.textContent = `You play "${inGame.player.toUpperCase()}" `;
+
+  let computer = document.createElement("span");
+  computer.setAttribute("id", "computer");
+  computer.textContent = `Computer play "${inGame.computer}"`;
+
+  li.appendChild(strong).appendChild(player).appendChild(computer);
+
+  document.querySelector(".round-group").appendChild(li);
+}
+
+/* Result functions */
+
+function ckeckResponses(playerResponse) {
+  inGame.player = playerResponse;
+  inGame.computer = getComputerCard();
+
+  let result = inGame.computer[playerResponse]; // return win, lose or egality
+  if (result == "win") inGame.score.win++;
+  if (result == "lose") inGame.score.lose++;
+  if (result == "egality") inGame.score.egality++;
+
+  displayScore();
+  displayRound();
+  displayCountRound(inGame.round);
+}
+
+/* Start function */
+
+function startTraditionalGame() {
+  gameInitialization();
+  domUpdateOnGameLaunch();
+}
